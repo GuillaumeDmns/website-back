08f2009d399e06988aeb77935299ab6498ad1133
Guillaume Damiens <guillaumedmns@gmail.com>
2022-11-28 00:20:49 +0100

    Add new fields in IDFMStop

--- src/main/java/com/gdamiens/website/model/IDFMStop.java
+++ src/main/java/com/gdamiens/website/model/IDFMStop.java
@@ -1,5 +1,6 @@
 package com.gdamiens.website.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -11,6 +12,17 @@ public class IDFMStop implements Serializable {
 
     @Id
     private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+
 
     public IDFMStop() {
     }
@@ -27,10 +39,37 @@ public class IDFMStop implements Serializable {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
     @Override
     public String toString() {
         return "IDFMStop{" +
             "id=" + id +
+            ", name='" + name + '\'' +
+            ", latitude=" + latitude +
+            ", longitude=" + longitude +
             '}';
     }
 }
