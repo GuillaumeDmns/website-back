193e2a0dd0d1d9736c81496d4069e9865e1f6c3d
Guillaume Damiens <guillaumedmns@gmail.com>
2022-12-04 00:00:54 +0100

    Refacto full IDFM request

--- src/main/java/com/gdamiens/website/controller/IDFMController.java
+++ src/main/java/com/gdamiens/website/controller/IDFMController.java
@@ -1,41 +1,27 @@
 package com.gdamiens.website.controller;
 
-import com.gdamiens.website.configuration.ApplicationProperties;
-import com.gdamiens.website.controller.object.Call;
 import com.gdamiens.website.controller.object.FullIDFMDTO;
 import com.gdamiens.website.controller.object.LineCSV;
 import com.gdamiens.website.controller.object.NextPassagesStop;
 import com.gdamiens.website.controller.object.StationAndLineCSV;
 import com.gdamiens.website.controller.object.StationCSV;
-import com.gdamiens.website.idfm.EstimatedCall;
-import com.gdamiens.website.idfm.IDFMResponse;
-import com.gdamiens.website.idfm.JourneyNote;
+import com.gdamiens.website.exceptions.CustomException;
 import com.gdamiens.website.model.IDFMLine;
 import com.gdamiens.website.service.CSVReader;
 import com.gdamiens.website.service.IDFMLineService;
 import com.gdamiens.website.service.IDFMStopService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -51,98 +37,43 @@ public class IDFMController {
 
     private static final String IDFM_ALL_LINES_URL = "https://data.iledefrance-mobilites.fr/explore/dataset/referentiel-des-lignes/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=;";
 
-    private final HttpComponentsClientHttpRequestFactory requestFactory;
-
-    private final ApplicationProperties applicationProperties;
-
     private final IDFMLineService idfmLineService;
 
     private final IDFMStopService idfmStopService;
 
-    public IDFMController(ApplicationProperties applicationProperties, IDFMLineService idfmLineService, IDFMStopService idfmStopService) {
-        this.requestFactory = new HttpComponentsClientHttpRequestFactory(HttpClients.custom().build());
-        this.applicationProperties = applicationProperties;
+    public IDFMController(IDFMLineService idfmLineService, IDFMStopService idfmStopService) {
         this.idfmLineService = idfmLineService;
         this.idfmStopService = idfmStopService;
     }
 
 
     @GetMapping("/fullIDFM")
-    @Operation(summary = "Get full next passages of all IDFM stops", security = @SecurityRequirement(name = "Auth. Token"))
-    public ResponseEntity<FullIDFMDTO> fullIDFM(String lineId) {
-
+    @Operation(summary = "Get full next passages of all IDFM stops by line", security = @SecurityRequirement(name = "Auth. Token"))
+    public ResponseEntity<FullIDFMDTO> getAllStopsByLine(String lineId) {
         try {
-            HttpHeaders headers = new HttpHeaders();
+            Map<Integer, NextPassagesStop> calls = this.idfmLineService.getAllStopsByLine(lineId, IDFM_ESTIMATED_TIMETABLE_URL);
 
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-            headers.set("Accept-encoding", "gzip, deflate");
-            headers.set("apiKey", applicationProperties.getIdfmKey());
+            IDFMLine idfmLine = this.idfmLineService.getLine(lineId);
 
-            HttpEntity<String> request = new HttpEntity<>(headers);
+            log.info("success requesting IDFM next passages");
 
-            UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(IDFM_ESTIMATED_TIMETABLE_URL)
-                .queryParam("LineRef", lineId != null ? "STIF:Line::" + lineId + ":" : "ALL");
+            return new ResponseEntity<>(new FullIDFMDTO(idfmLine, calls), HttpStatus.OK);
+        } catch (CustomException e) {
+            log.info(e.getMessage());
 
-            ResponseEntity<IDFMResponse> response = new RestTemplate(this.requestFactory).exchange(uriComponentsBuilder.build().toUri(), HttpMethod.GET, request, IDFMResponse.class);
-
-            if (response.getStatusCode().is2xxSuccessful()) {
-                IDFMResponse idfmResponse = response.getBody();
-
-                if (idfmResponse != null) {
-                    IDFMLine idfmLine = this.idfmLineService.getLine(lineId);
-
-                    Map<Integer, NextPassagesStop> calls = idfmResponse
-                        .getSiri()
-                        .getServiceDelivery()
-                        .getEstimatedTimetableDelivery()
-                        .get(0)
-                        .getEstimatedJourneyVersionFrame()
-                        .get(0)
-                        .getEstimatedVehicleJourney()
-                        .stream()
-                        .filter(estimatedVehicleJourney -> estimatedVehicleJourney.getEstimatedCalls() != null && !estimatedVehicleJourney.getEstimatedCalls().getEstimatedCall().isEmpty())
-                        .flatMap(estimatedVehicleJourney -> estimatedVehicleJourney
-                            .getEstimatedCalls()
-                            .getEstimatedCall()
-                            .stream()
-                            .peek(call -> {
-                                call.setRecordedAtTime(estimatedVehicleJourney.getRecordedAtTime());
-                                List<JourneyNote> journeyNote = estimatedVehicleJourney.getJourneyNote();
-                                call.setJourneyNote(journeyNote != null && !journeyNote.isEmpty() ? journeyNote.get(0).getValue() : null);
-                                call.setFirstOrLastJourney(estimatedVehicleJourney.getFirstOrLastJourney());
-                            })
-                        )
-                        .filter(estimatedCall -> !estimatedCall.getDestinationDisplay().isEmpty())
-                        .collect(Collectors.groupingBy(estimatedCall -> Integer.parseInt(estimatedCall.getStopPointRef().getValue().split(":")[3])))
-                        .entrySet()
-                        .stream()
-                        .collect(Collectors.toMap(
-                            Map.Entry::getKey,
-                            e -> new NextPassagesStop(this.idfmStopService.getStop(e.getKey()), e.getValue()
-                                .stream()
-                                .map(Call::new)
-                                .collect(Collectors.groupingBy(Call::getDirectionName)))
-                        ));
-//                                    ,this.idfmStopService.getStop(Integer.parseInt(estimatedCall.getStopPointRef().getValue().split(":")[3])))
-
-                    return new ResponseEntity<>(new FullIDFMDTO(idfmLine, calls), HttpStatus.OK);
-                }
-
-                log.info("success requesting IDFM");
-            }
-        } catch (Exception e) {
+            return new ResponseEntity<>(e.getHttpStatus());
+        }
+        catch (Exception e) {
             log.info("error during IDFM next passages request");
         }
 
-        return new ResponseEntity<>(new FullIDFMDTO(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @GetMapping("/update-stations-and-lines")
     @Operation(summary = "Get list of all stops and lines", security = @SecurityRequirement(name = "Auth. Token"))
     public ResponseEntity<Void> updateStopsAndLines() {
         try {
-
             CSVReader<StationAndLineCSV> csvReader = new CSVReader<>(StationAndLineCSV.class);
 
             Map<String, List<StationAndLineCSV>> linesAndStops = csvReader.readFromUrl(IDFM_ALL_STATIONS_AND_LINES_URL, StationAndLineCSV::getLineRef);
--- src/main/java/com/gdamiens/website/service/IDFMLineService.java
+++ src/main/java/com/gdamiens/website/service/IDFMLineService.java
@@ -1,14 +1,31 @@
 package com.gdamiens.website.service;
 
+import com.gdamiens.website.configuration.ApplicationProperties;
+import com.gdamiens.website.controller.object.Call;
 import com.gdamiens.website.controller.object.LineCSV;
+import com.gdamiens.website.controller.object.NextPassagesStop;
 import com.gdamiens.website.controller.object.StationAndLineCSV;
+import com.gdamiens.website.exceptions.CustomException;
+import com.gdamiens.website.idfm.IDFMResponse;
+import com.gdamiens.website.idfm.JourneyNote;
 import com.gdamiens.website.model.IDFMLine;
 import com.gdamiens.website.model.IDFMStopLine;
 import com.gdamiens.website.repository.IDFMLineRepository;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -24,10 +41,74 @@ public class IDFMLineService {
 
     private final IDFMLineRepository idfmLineRepository;
 
-    public IDFMLineService(IDFMStopService idfmStopService, IDFMStopLineService idfmStopLineService, IDFMLineRepository idfmLineRepository) {
+    private final HttpComponentsClientHttpRequestFactory requestFactory;
+
+    private final ApplicationProperties applicationProperties;
+
+    public IDFMLineService(IDFMStopService idfmStopService, IDFMStopLineService idfmStopLineService, IDFMLineRepository idfmLineRepository, ApplicationProperties applicationProperties) {
         this.idfmStopService = idfmStopService;
         this.idfmStopLineService = idfmStopLineService;
         this.idfmLineRepository = idfmLineRepository;
+        this.requestFactory = new HttpComponentsClientHttpRequestFactory(HttpClients.custom().build());
+        this.applicationProperties = applicationProperties;
+    }
+
+    public Map<Integer, NextPassagesStop> getAllStopsByLine(String lineId, String url) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("Accept-encoding", "gzip, deflate");
+        headers.set("apiKey", applicationProperties.getIdfmKey());
+
+        HttpEntity<String> request = new HttpEntity<>(headers);
+
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(url)
+            .queryParam("LineRef", lineId != null ? "STIF:Line::" + lineId + ":" : "ALL");
+
+        ResponseEntity<IDFMResponse> response = new RestTemplate(this.requestFactory).exchange(uriComponentsBuilder.build().toUri(), HttpMethod.GET, request, IDFMResponse.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            IDFMResponse idfmResponse = response.getBody();
+
+            if (idfmResponse != null) {
+
+                return idfmResponse
+                    .getSiri()
+                    .getServiceDelivery()
+                    .getEstimatedTimetableDelivery()
+                    .get(0)
+                    .getEstimatedJourneyVersionFrame()
+                    .get(0)
+                    .getEstimatedVehicleJourney()
+                    .stream()
+                    .filter(estimatedVehicleJourney -> estimatedVehicleJourney.getEstimatedCalls() != null && !estimatedVehicleJourney.getEstimatedCalls().getEstimatedCall().isEmpty())
+                    .flatMap(estimatedVehicleJourney -> estimatedVehicleJourney
+                        .getEstimatedCalls()
+                        .getEstimatedCall()
+                        .stream()
+                        .peek(call -> {
+                            call.setRecordedAtTime(estimatedVehicleJourney.getRecordedAtTime());
+                            List<JourneyNote> journeyNote = estimatedVehicleJourney.getJourneyNote();
+                            call.setJourneyNote(journeyNote != null && !journeyNote.isEmpty() ? journeyNote.get(0).getValue() : null);
+                            call.setFirstOrLastJourney(estimatedVehicleJourney.getFirstOrLastJourney());
+                        })
+                    )
+                    .filter(estimatedCall -> !estimatedCall.getDestinationDisplay().isEmpty())
+                    .collect(Collectors.groupingBy(estimatedCall -> Integer.parseInt(estimatedCall.getStopPointRef().getValue().split(":")[3])))
+                    .entrySet()
+                    .stream()
+                    .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> new NextPassagesStop(this.idfmStopService.getStop(e.getKey()), e.getValue()
+                            .stream()
+                            .map(Call::new)
+                            .collect(Collectors.groupingBy(Call::getDirectionName)))
+                    ));
+            }
+        }
+
+        throw new CustomException("IDFM response != 200 or body is null", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     public void refreshLinesAndStops(Map<String, List<StationAndLineCSV>> linesAndStops) {
