b342259de9bec762b3415be52b7dad88b8846204
Guillaume Damiens <guillaumedmns@gmail.com>
2023-01-15 01:38:27 +0100

    (Finally...) Order list of lines

--- src/main/java/com/gdamiens/website/service/IDFMLineService.java
+++ src/main/java/com/gdamiens/website/service/IDFMLineService.java
@@ -34,6 +34,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
